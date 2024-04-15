023558754079d5df5e1f979f9a490ea4601acaf9
Sergio de Morais <srdemorais.sandbox@gmail.com>
2024-04-15 23:46:50 +0200

    add server routes

--- pages/about.vue
+++ pages/about.vue
@@ -3,11 +3,14 @@
     <h2>About</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nemo veritatis sed fugit. Incidunt nisi, animi assumenda voluptas aliquid temporibus laudantium eius tempora tenetur labore! Quisquam necessitatibus inventore dignissimos aut cum repellat, expedita, ut, est sunt aperiam excepturi ratione quaerat explicabo itaque unde? Quae, obcaecati. Amet corrupti assumenda libero ducimus.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque beatae labore expedita rem voluptas earum eaque? Qui dolore cum rem voluptate architecto saepe possimus, est praesentium quaerat sed dicta nam accusantium earum totam modi laboriosam consectetur ipsam ipsum quos nostrum error non. Incidunt tempore sint porro enim culpa quod ea?</p>
+    <div>
+      {{ data }}
+    </div>
   </div>
 </template>
 
 <script setup>
-
+  const { data } = await useFetch('/api/currency')
 </script>
 
 <style scoped>
