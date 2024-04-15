8e64190d75928712714909ac48db1007cea19037
Sergio de Morais <srdemorais.sandbox@gmail.com>
2024-04-15 15:05:46 +0200

    add tailwind css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/">Nuxt shop</NuxtLink>
-        <ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt shop</NuxtLink>
+        <ul class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
         </ul>
       </nav>
     </header>
   </div>
 
   <!-- page content -->
-  <div>
+  <div class="container mx-auto p-4">
     <slot />
   </div>
 </template>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/products">Nuxt products</NuxtLink>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Nuxt products</NuxtLink>
       </nav>
     </header>
   </div>
 
   <!-- page content -->
-  <div>
+  <div class="container mx-auto p-4">
     <slot />
   </div>
 
   <div>
-    <footer>
-      <ul>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>
