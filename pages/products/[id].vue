6244890321aca3797eff9665919ef9d0b22fe8b5
Sergio de Morais <srdemorais.sandbox@gmail.com>
2024-04-15 20:47:18 +0200

    add error pages

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -11,6 +11,10 @@
 
   const { data: product } = await useFetch(uri, { key: id })
 
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: "Product not found", fatal: true })
+  }
+
   definePageMeta({
     layout: 'products'
   })
